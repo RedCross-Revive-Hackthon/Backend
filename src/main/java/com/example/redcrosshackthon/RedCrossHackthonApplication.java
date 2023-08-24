b5574be1316db0cdb3beaf93cd5413e038604abd
SeoYoung <seoyoung7623@naver.com>
2023-08-24 17:24:45 +0900

    feat: springApplication 시간저장 설정

--- src/main/java/com/example/redcrosshackthon/RedCrossHackthonApplication.java
+++ src/main/java/com/example/redcrosshackthon/RedCrossHackthonApplication.java
@@ -2,9 +2,15 @@ package com.example.redcrosshackthon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RedCrossHackthonApplication {
+
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RedCrossHackthonApplication.class, args);
