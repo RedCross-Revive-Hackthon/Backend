84334d515efadf0bd8f2c4a7c527155d8938ff85
SeoYoung <seoyoung7623@naver.com>
2023-08-24 17:25:33 +0900

    feat: 유저 마이페이지 조회

--- src/main/java/com/example/redcrosshackthon/domain/user/controller/UserController.java
+++ src/main/java/com/example/redcrosshackthon/domain/user/controller/UserController.java
@@ -1,26 +1,21 @@
 package com.example.redcrosshackthon.domain.user.controller;
 
-import com.example.redcrosshackthon.domain.user.dto.request.UserRegisterReqDto;
 import com.example.redcrosshackthon.domain.user.dto.response.UserInfoResDto;
 import com.example.redcrosshackthon.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
-
-    @PostMapping("/signup")
-    public ResponseEntity<UserInfoResDto> signup(@RequestBody UserRegisterReqDto userRegisterReqDto){
-        UserInfoResDto userInfoDto = userService.signup(userRegisterReqDto);
-        return ResponseEntity.status(HttpStatus.OK).body(userInfoDto);
+    @GetMapping("/{user_id}")
+    public ResponseEntity<UserInfoResDto> getUser(@PathVariable("user_id") Long userId){
+        UserInfoResDto userInfoResDto = userService.getUser(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userInfoResDto);
     }
 
 }
--- src/main/java/com/example/redcrosshackthon/domain/user/entity/User.java
+++ src/main/java/com/example/redcrosshackthon/domain/user/entity/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,13 +17,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String user_id;
+
+    private String pwd;
+
     private String name;
 
     private String email;
 
+    private String department;
+
+    private String image;
+
     @Builder
-    public User(String name, String email) {
+    public User(String user_id, String pwd, String name, String email, String department) {
+        this.user_id = user_id;
+        this.pwd = pwd;
         this.name = name;
         this.email = email;
+        this.department = department;
     }
 }
