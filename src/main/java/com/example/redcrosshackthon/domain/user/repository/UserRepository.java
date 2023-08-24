1c7fcfb54210c7f703ffda920d49dfba5c37c0af
SeoYoung <seoyoung7623@naver.com>
2023-08-25 00:04:06 +0900

    feat: 유저 랭크 조회기능

--- src/main/java/com/example/redcrosshackthon/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/redcrosshackthon/domain/user/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.redcrosshackthon.domain.user.repository;
 
+import com.example.redcrosshackthon.domain.user.dto.response.RankUserResDto;
 import com.example.redcrosshackthon.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u from User u join u.scores")
+    List<RankUserResDto> getUserScores();
 }
